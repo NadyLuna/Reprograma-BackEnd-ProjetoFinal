be5567fa96b6f328421e228e8676a13a2f784349
NadyLuna <nadyedja@gmail.com>
2020-12-01 16:20:48 -0300

    criando rota de read alunos

--- src/routes/alunosRouter.js
+++ src/routes/alunosRouter.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/alunosController.js");
 
+router.get("/",controller.readAll);
+
+module.exports = router;
