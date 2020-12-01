fd9aacb1d58a6aa4eee5910a0700415a8ba44cc0
NadyLuna <nadyedja@gmail.com>
2020-12-01 16:24:12 -0300

    incluindo rota create alunos

--- src/routes/alunosRouter.js
+++ src/routes/alunosRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/alunosController.js");
 
 router.get("/",controller.readAll);
+router.post("/",controller.create)
 
 module.exports = router;
