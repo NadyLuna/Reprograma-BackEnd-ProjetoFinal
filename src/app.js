6d98061d32615a100bcec7a84bb3fc8589c9b94c
NadyLuna <nadyedja@gmail.com>
2020-12-03 22:44:44 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
