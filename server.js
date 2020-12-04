bb29df636a1736840e5520bfad5c7139958cb8df
NadyLuna <nadyedja@gmail.com>
2020-12-03 22:34:43 -0300

    ajust porta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./src/app");
-const port = process.env.PORT || '3000';
+const app = require("./src/app")
+const port = 3000
 
-app.listen(port, () => {
-    console.log(`O App está rodando na porta ${port}`)
-});
+app.listen(process.env.PORT || port, () => {
+    console.log(`App está rodando na porta ${port}`)
+})
