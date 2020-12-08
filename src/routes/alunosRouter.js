0ab8757a0a10e22f5e4c1e82883378297bd09bd3
NadyLuna <nadyedja@gmail.com>
2020-12-08 17:08:56 -0300

    ajuste de hierarquia de rota

--- src/routes/alunosRouter.js
+++ src/routes/alunosRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const controller = require("../controllers/alunosController.js");
 
 router.get("/",controller.selectAll);
-router.get("/:cpf",controller.selectById);
 router.post("/",controller.insertStudant);
+router.get("/:cpf",controller.selectById);
 router.put("/:cpf",controller.updateStudant);
 router.patch("/:cpf",controller.updateStudant);
 router.delete("/:cpf",controller.deleteStudant);
