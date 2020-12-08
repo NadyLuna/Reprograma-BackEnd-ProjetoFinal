75d2e09b3cbd223c6cdf27dc52b4889a1ea5c562
NadyLuna <nadyedja@gmail.com>
2020-12-08 17:07:57 -0300

    inclusão de rotas update e delete

--- src/routes/faculdadesRouter.js
+++ src/routes/faculdadesRouter.js
@@ -5,5 +5,7 @@ const controller = require("../controllers/faculdadesController");
 
 router.post("/",controller.create)
 router.get("/",controller.SelectAll)
+router.put("/:id", controller.updateFaculdade)
+router.delete("/:id",controller.deleteFaculdade)
 
 module.exports = router;
