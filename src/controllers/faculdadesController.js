05fe3012bf0612a87795fdb90ffefb91c4d9bf57
NadyLuna <nadyedja@gmail.com>
2020-12-08 17:07:20 -0300

    Inclusão de Update e Delete

--- src/controllers/faculdadesController.js
+++ src/controllers/faculdadesController.js
@@ -3,10 +3,14 @@ const {faculdadesModel} = require('../models/faculdades')
 
 const create = (req, res) => {
     const newfaculdade = new faculdadesModel(req.body)
+
     newfaculdade.save(function (err) {
+
         if (err) {
             return res.status(424).send({message: err.message})
+
         } else {
+
             return res.status(201).send({ message: "Faculdade incluída com sucesso",})
         }
     })
@@ -22,15 +26,67 @@ const SelectAll = (req, res) => {
     })
 }
 
+const SelectById = (req, res) => {
+    const id = req.params.id;
 
+    faculdadesModel.find({ _id: id }, (err, faculdade) => {
 
+        if (err) {
+            return res.status(424).send({ message: err.message});
 
+        } else if (faculdade.length > 0) {
+            return res.status(200).send(faculdade);
 
+        } else {
+            return res.status(404).send({ message: "Faculdade não encontrada com este ID.",
+            });
+        };
+    });
+};
 
+const updateFaculdade = (req, res) => {
+    const id = req.params.id;
+    faculdadesModel.find({ _id: id}, (err, facul) => {
+    
+        if( facul > 0 ) {
+
+            return res.status(404).send("Não há faculdade com este ID para ser atualizada");
+        }else {
+            faculdadesModel.updateOne({ _id: id}, {$set: req.body}, function (err){
+
+                if(err) {
+                return res.status(500).send({message: err.message});
+            }
+                return res.status(200).send({message: "Faculdade atualizada com sucesso"});
+            });        
+        };
+    });
+};
+
+const deleteFaculdade = (req, res) => {
+    const id = req.params.idFaculdade;
+    faculdadesModel.find({idFaculdade}, (err, faculdade) => {
+        if (faculdade.length > 0) {
+            faculdadesModel.deleteMany({Idfaculdade}, (err) => {
+                if(err) {
+                return res.status(424).send({message: err.message})
+            }   
+                return res.status(200).send({status: true, mensagem: "Faculdade excluída com Sucesso", });
+            });
+
+                } else {
+                return res.status(404).send("Faculdade não encontrada");
+        }
+    });
+};
 
 module.exports = {
     create,
-    SelectAll
+    SelectAll,
+    SelectById,
+    updateFaculdade,
+    deleteFaculdade
+
 }
 
 
