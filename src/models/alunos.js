97364ad8f1c4fc7cd318678172591c9d9317fa23
NadyLuna <nadyedja@gmail.com>
2020-12-11 20:17:25 -0300

    alteração de schema

--- src/models/alunos.js
+++ src/models/alunos.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-const{ faculdadesSchema } = require('./faculdades')
+
 
 const alunosSchema = new mongoose.Schema({
-    
-    idaluno: { type: Number},
+
     email: { type: String,lowercase: true,required: true, validate: [isEmail, 'Email inválido'], unique: true },
     senha: { type: Number,lowercase: true,required: true},
-    faculdade: [faculdadesSchema]
+    cpf: { type: String }
 }, {
     versionKey: false
 });
--- src/models/faculdades.js
+++ src/models/faculdades.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const {alunosSchema} = require('./alunos')
 const faculdadesSchema = new mongoose.Schema({
 
-    idFaculdade: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
     codigo: { type: String},
     nomeFaculdade: { type: String },
     nomeAluno: { type: String},
-    cpf: { type: String },
     matriculado: { type: Boolean},
     alunos: [alunosSchema]    
     
